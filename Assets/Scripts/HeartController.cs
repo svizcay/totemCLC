a7d42010daf8d8d311fb994198baee1b16a4bf08
Sebastian Vizcay <svizcay@gmail.com>
2016-03-22 18:38:23 -0400

    initial commit

---
+++ Assets/Scripts/HeartController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeartController : MonoBehaviour {
+
+	public int nrOverlayedItems = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		nrOverlayedItems++;
+		// print ("nr overlayed items: " + nrOverlayedItems);
+	}
+
+	void OnTriggerExit2D(Collider2D other)
+	{
+		nrOverlayedItems--;
+	}
+}
---
+++ Assets/Scripts/MainController.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainController : MonoBehaviour {
+
+	public int nrItems = 1000;
+	public int maxPositiveHorizontalSize = 9;
+	public int maxPositiveVerticalSize = 4;
+	public int nrHearts = 3;
+
+	public GameObject heartPrefab;
+	public GameObject emptyItemPrefab;
+
+	public Transform heartsContainerTransform;
+	public Transform itemsContainerTransform;
+
+	public Sprite[] sprites;
+
+	private int nrVisibleHearts = 0;
+	private bool won = false;
+	private bool isPlaying = true;
+
+	private GameObject[] hearts;
+
+	// Use this for initialization
+	void Start () {
+		// instantiate hearts
+		hearts = new GameObject[nrHearts];
+		for (int i = 0; i < nrHearts; i++) {
+			Vector2 randomPosition = new Vector2 (Random.Range (-maxPositiveHorizontalSize, maxPositiveHorizontalSize+1), Random.Range (-maxPositiveVerticalSize, maxPositiveVerticalSize));
+			GameObject newHeart = Instantiate (heartPrefab, randomPosition, Quaternion.identity) as GameObject;
+			newHeart.transform.parent = heartsContainerTransform;
+			hearts [i] = newHeart;
+		}
+
+		// instantiate items
+		for (int i = 0; i < nrItems; i++) {
+			int randomSpriteNr = Random.Range (0, sprites.Length);
+			Sprite randomSprite = sprites [randomSpriteNr];
+			Vector2 randomPosition = new Vector2 (Random.Range (-maxPositiveHorizontalSize, maxPositiveHorizontalSize+1), Random.Range (-maxPositiveVerticalSize, maxPositiveVerticalSize));
+			GameObject newPrefab = Instantiate (emptyItemPrefab, randomPosition, Quaternion.identity) as GameObject;
+			SpriteRenderer spriteRenderer = newPrefab.GetComponent<SpriteRenderer> ();
+			spriteRenderer.sprite = randomSprite;
+			newPrefab.transform.parent = itemsContainerTransform;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isPlaying) {
+			// check if a new heart is visible
+			for (int i = 0; i < nrHearts; i++) {
+				BoxCollider2D heartCollider = hearts [i].GetComponent<BoxCollider2D> ();
+				if (heartCollider) {
+					HeartController heartController = hearts [i].GetComponent<HeartController> (); 
+					if (heartController.nrOverlayedItems == 0) {
+						// heart is visible
+						nrVisibleHearts++;
+						print ("heart is now visible");
+						heartCollider.isTrigger = false;
+					}
+				}
+
+
+			}
+
+			if (nrVisibleHearts == nrHearts) {
+				won = true;
+				isPlaying = false;
+			}
+		}
+	}
+}
